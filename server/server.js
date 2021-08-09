9942c45025b10eb4771a4bab594c6f3f20ceb401
williamtuominiemi <william.m.tuominiemi@gmail.com>
2021-08-09 17:15:05 +0300

    switched port

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
-const io = require('socket.io')(3000 ,{
+const io = require('socket.io')(8080 ,{
     cors: {
-        origin:['http://localhost:8080']
+        origin:['http://localhost:3000']
     }
 })
 
 io.on('connect', socket => {
-    console.log(socket.id)
     socket.on("send-message", (message) => {
         socket.broadcast.emit('receive-message', message)
     })
