5a2e59ef65bcecd2060f23cdf984aa5274046a20
williamtuominiemi <william.m.tuominiemi@gmail.com>
2021-08-14 12:20:12 +0300

    create room form

--- client/src/components/main.component.js
+++ client/src/components/main.component.js
@@ -4,21 +4,66 @@ import { Link } from 'react-router-dom'
 // Bootstrap
 import { Card, Form, Button, FloatingLabel, Dropdown } from 'react-bootstrap';
 
-export default class PostsList extends Component {
-//   constructor(props) {
-//     super(props)
-//   }
+export default class Main extends Component {
+    constructor(props) {
+        super(props)
 
-//   componentDidMount() {
+        this.onChangeRoomName = this.onChangeRoomName.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+        
+        this.state = {
+            roomName: '',
+        }
+    }
 
-//   }
-    
-  render() {
+    onChangeRoomName(e) {
+        this.setState({
+            roomName: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+       
+        console.log(e)
+
+        this.setState({
+            roomName: ""
+        })
+    }
+  
+    render() {
     return (
         <Card>
             <Card.Title>ChatApp</Card.Title>
             <Card.Body>
-                <Button href="/chat">Chat</Button>
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Form onSubmit={this.onSubmit}>
+                        <Form.Group className="mb-3" controlId="formMessage">
+                            <FloatingLabel controlId="floatingMessage" label="room name" style={{margin: "10px"}}>
+                                <Form.Control 
+                                    type="text" 
+                                    required
+                                    className="form-control"
+                                    placeholder="room name"
+                                    value={this.state.roomName}
+                                    onChange={this.onChangeRoomName}
+                                />
+                            </FloatingLabel>
+                            <Button variant="primary" type="submit" style={{margin: "10px"}}>
+                                Create room
+                            </Button>
+                        </Form.Group>
+                    </Form>
+                </Dropdown.Menu>
+            </Dropdown>    
+            <br/>
+            <Button href="/chat">Chat</Button>
             </Card.Body>
         </Card>
     )
