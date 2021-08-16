9d6654f69953b15a89e59a3bf6c4c2e94438abc5
williamtuominiemi <william.m.tuominiemi@gmail.com>
2021-08-16 21:43:40 +0300

    go through each room and get the amount of users in it

--- server/server.js
+++ server/server.js
@@ -22,10 +22,31 @@ io.on('connect', socket => {
     // console.log(socket.id)
 
     const loadRooms = () => {
-        console.log("Load rooms")
+        // console.log("Load rooms")
         Room.find()
         .then((result) => {
-            io.to(socket.id).emit('get-rooms', result)
+            const checkAmountOfUsers = new Promise((resolve, reject) => {
+                result.forEach((value, index, array) => {
+                    // console.log(value._id.toString());
+                    // console.log(io.sockets.adapter.rooms.get(value._id.toString()))
+                    if(io.sockets.adapter.rooms.get(value._id.toString())) {
+                        // console.log(io.sockets.adapter.rooms.get(value._id.toString()).size)
+                        result[index].usersOnline = io.sockets.adapter.rooms.get(value._id.toString()).size
+                    } else {
+                        // console.log('unPOG') 
+                        result[index].usersOnline = 0
+                        console.log(result[index])
+                    }
+
+                    // result[index].usersOnline = io.sockets.adapter.rooms.get(result[index]._id).size
+                    if (index === array.length -1) resolve();
+                });
+            });
+            
+            checkAmountOfUsers.then(() => {
+                // console.log(result)
+                io.to(socket.id).emit('get-rooms', result)
+            });
         })
     }   
 
@@ -48,7 +69,8 @@ io.on('connect', socket => {
     socket.on("join-room", room => {
         // console.log('joined room'+room)
         socket.join(room)
-        console.log(io.sockets.adapter.rooms.get(room).size)
+        // console.log(room)
+        // console.log(io.sockets.adapter.rooms.get(room).size)
     })
 
     loadRooms()
